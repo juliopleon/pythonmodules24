a6ca2a7066f18eee372b2d61d6a7bb340a4986b6
Julio Leon <juliopena143@icloud.com>
2024-09-11 12:09:09 -0600

    importing and creating packages and sub packages

--- myprogram.py
+++ myprogram.py
@@ -1,3 +1,11 @@
+# Import from a file(module)
 from mymodule import my_func
-
+# Import from a Main-Package folder
+from MyMainPackage import some_main_script
+# Import from Sub-Package Folder
+from MyMainPackage.SubPackage import mysubscript
 my_func()
+
+some_main_script.report_main()
+
+mysubscript.sub_report()
