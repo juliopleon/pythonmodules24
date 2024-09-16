316485a81068da8f0589cd4efc5f8f38cf709b89
Julio Leon <juliopena143@icloud.com>
2024-09-16 14:52:26 -0600

    notes

--- Unittest Library/test_cap.py
+++ Unittest Library/test_cap.py
@@ -1,9 +1,13 @@
+# import unittest and script u want to test
 import unittest
 import cap
+
+# create a test class
 
 
 class test_cap(unittest.TestCase):
 
+    # test methods to make sure scripts are working
     def test_one_word(self):
         text = 'python'
         result = cap.cap_text(text)
